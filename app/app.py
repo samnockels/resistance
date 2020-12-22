26afe9a87487660df6851b04fdda7f7bd957a07c
alice-dotcom <ninjasniglet@protonmail.com>
2020-12-22 17:40:20 +0000

    Initilaise player order, pick crew, vote on crew

--- app/app.py
+++ app/app.py
@@ -6,13 +6,10 @@ import os
 app = Flask(__name__)
 store.init_game_master_pass(os.environ['GAME_MASTER_PASS'])
 
-def format(data):
-  return json.dumps(data)
-
 @app.route('/', methods=['GET'])
 def hello_world():
   return jsonify({
-    "players": resistance.get_players()
+    "players": store.get_players()
   })
 
 @app.route('/enter/<string:name>')
@@ -24,3 +21,19 @@ def enter(name):
 @app.route('/assign')
 def assign(): 
   return jsonify(resistance.assign_roles())
+
+@app.route('/player_order')
+def initialise_player_order(): 
+  return jsonify(resistance.initialise_player_order())
+
+@app.route('/crew/<string:crew_member_ids>')
+def crew_select(crew_member_ids): 
+  return jsonify(resistance.crew_selection(crew_member_ids))
+
+@app.route('/vote/<string:vote>')
+def crew_vote(vote): 
+  return jsonify(resistance.vote_on_crew(vote))
+
+@app.route('/test')
+def test():
+  return jsonify([store.get_number_votes_for_crew()])
