bf5b8ec01215213612487b460b1a56d0b5afe170
samnockels <samnockels@gmail.com>
2020-12-30 16:31:16 +0000

    join room functionality

--- app/server.py
+++ app/server.py
@@ -1,9 +1,10 @@
-from flask_socketio import emit, join_room, leave_room
-from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask import Flask, request
 import routes.auth
+import routes.game
 import store
 import os
+import crypto
 # from events import register_events
 
 socketio = SocketIO()
@@ -16,8 +17,8 @@ store.init_game_master_pass(os.environ['GAME_MASTER_PASS'])
 
 # Register http routes
 app.register_blueprint(routes.auth.app, url_prefix='/')
-# app.register_blueprint(routes.game.app, url_prefix='/game')
-# app.register_blueprint(routes.player.app, url_prefix='/player')
+app.register_blueprint(routes.game.app, url_prefix='/game')
+app.register_blueprint(routes.player.app, url_prefix='/player')
 
 
 @app.after_request
@@ -32,9 +33,58 @@ def apply_cors_headers(response):
 #
 
 
+clients = {}
+
+
+def socketError(message):
+    emit('error', message)
+    raise Exception(message)
+
+
+def socketGetPlayer(req):
+    player_id = crypto.decrypt(req.get('auth'))
+    player = store.get_player(player_id)
+    if not player:
+        socketError('socketGetPlayer - player not found')
+    if not clients.get(player_id):
+        clients[player_id] = player
+    # update the sid
+    clients[player_id]['sid'] = request.sid
+    return player
+
+
 @socketio.event
-def echo(data):
-    emit('echoreply', data)
+def hello(req):
+    player = socketGetPlayer(req)
+    emit('welcome', 'Welcome ' + player.get('name'))
+    emit('clients-updated', clients)
+
+
+@socketio.event
+def echo(req):
+    emit('echoreply', req)
+
+
+@socketio.event
+def join(req):
+    player = socketGetPlayer(req)
+    data = req.get('data')
+    room = data.get('room')
+    if not room:
+        socketError('room is a required field')
+    join_room(data.get('room'))
+    emit('onJoin', player.get('name') + ' joined the room.', room=room)
+
+
+@socketio.event
+def leave(req):
+    player = socketGetPlayer(req)
+    data = req.get('data')
+    room = data.get('room')
+    if not room:
+        socketError('room is a required field')
+    leave_room(data.get('room'))
+    emit('onLeave', player.get('name') + ' left the room', room=request.sid)
 
 
 socketio.init_app(app, cors_allowed_origins="*")
